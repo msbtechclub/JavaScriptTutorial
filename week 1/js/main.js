621791a6bc4fcf524e7d5ee1fa9efcc10988061e
msbtechclub <msbtechclub@gmail.com>
2017-09-07 12:50:06 +0200

    first week

---
+++ week 1/js/main.js
@@ -0,0 +1,10 @@
+function sum() {
+	var num1 = parseInt(document.getElementById('n1').value);
+	var num2 = parseInt(document.getElementById('n2').value);
+	var sum = num1 + num2;
+	document.getElementById('result').innerHTML = num1 + num2;
+}
+
+function pop () {
+	window.open("https://www.google.com");
+}
