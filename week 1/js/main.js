eff30e113445b91ad385e104f714eae6b6c79342
msbtechclub <msbtechclub@gmail.com>
2017-09-09 13:07:47 +0200

    New function added

--- week 1/js/main.js
+++ week 1/js/main.js
@@ -1,10 +1,35 @@
 function sum() {
+	/*
+	two variables declared num1 and num2
+	their values are set to the value of the inputs in the index.html file with id n1 and n2
+	and then the values are converted from string to integer using embeded
+	function parseInt().
+	*/
+
 	var num1 = parseInt(document.getElementById('n1').value);
 	var num2 = parseInt(document.getElementById('n2').value);
-	var sum = num1 + num2;
+	// setting the inner value of the tag which contains the id equals to result.
 	document.getElementById('result').innerHTML = num1 + num2;
 }
 
-function pop () {
-	window.open("https://www.google.com");
+function calculate() {
+	var num1 = document.getElementById('n1').value;
+	var num2 = document.getElementById('n2').value;
+	var operator = document.getElementById('operator').value;
+	if(num1 === "" || num2 === "") {
+		alert("fill empty fields");
+	} else {
+		num1 = parseInt(document.getElementById('n1').value);
+		num2 = parseInt(document.getElementById('n2').value);
+		if(operator === '+') {
+			document.getElementById('result').innerHTML = num1 + num2;
+		} else if(operator === '-') {
+			document.getElementById('result').innerHTML = num1 - num2;
+		} else if(operator === '*') {
+			document.getElementById('result').innerHTML = num1 * num2;
+		} else if(operator === '/') {
+			document.getElementById('result').innerHTML = num1 / num2;
+		}
+	}
+	
 }
